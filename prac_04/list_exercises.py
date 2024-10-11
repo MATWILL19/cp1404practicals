3ed956ef81093a7b116115e1f8dea0c1fb96752c
MATWILL19 <mathew.willington@my.jcu.edu.au>
2024-10-11 10:42:38 +1000

    Complete woefully inadequate security checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,3 +1,4 @@
+"""1. Basic List Ops"""
 def main():
     """Get numbers into a list and then display them"""
     numbers = get_numbers()
@@ -20,3 +21,24 @@ def display_numbers(numbers):
     print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
 
 main()
+
+"""Woefully Inadequate Security Checker"""
+USERNAMES = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+def main():
+    """Get a username and compare it against a list"""
+    username = input("Enter username: ")
+    access = compare_username(username)
+    print(access)
+
+def compare_username(username):
+    """Compare username against list"""
+    if username in USERNAMES:
+        access_status = "Access granted"
+        return access_status
+    else:
+        access_status = "Access denied - releasing killbots"
+        return access_status
+main()
+
