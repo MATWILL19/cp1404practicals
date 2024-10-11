3025a530d2066ea1a7d6f5154ebabbdba7a8e1bb
MATWILL19 <mathew.willington@my.jcu.edu.au>
2024-10-11 10:09:09 +1000

    Complete list exercises

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,2 +1,22 @@
 def main():
-    """This program will: Get numbers into a list and then display"""
+    """Get numbers into a list and then display them"""
+    numbers = get_numbers()
+    display_numbers(numbers)
+
+def get_numbers():
+    """Get numbers in to list"""
+    numbers = []
+    for i in range(5):
+        number = int(input('Enter a number: '))
+        numbers.append(number)
+    return numbers
+
+def display_numbers(numbers):
+    """Display numbers"""
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
+
+main()
