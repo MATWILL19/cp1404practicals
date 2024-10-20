c026e4cca97a75b21b843e34a78567daa095e45f
MATWILL19 <mathew.willington@my.jcu.edu.au>
2024-10-20 21:15:48 +1000

    Finish word occurences program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,14 +1,16 @@
 """
 Word Occurrences
 Estimate: 30 minutes
-Actual:   32 minutes
+Actual:   50 minutes
 """
 word_to_count = {}
 
-words = input("Please enter a sentence: ")
-parts = words.split(" ")
-for part in parts:
+word_input = input("Please enter a sentence: ")
+words = word_input.split(" ")
+for part in words:
     word_to_count[part] = word_to_count.get(part, 0) + 1
-for word in word_to_count:
-    width = 10
-    print(f"{word:{width}} : {word_to_count[word]}")
+words = list(word_to_count.keys())
+words.sort()
+word_length = max((len(word) for word in word_input))
+for word in words:
+    print(f"{word:{word_length}} : {word_to_count[word]}")
