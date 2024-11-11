713fb4fa0a62229fee098582969e736be0a78f92
MATWILL19 <mathew.wilington@my.jcu.edu.au>
2024-11-11 18:04:11 +1000

    Add display projects, load projects and display projects functions

--- prac_07/project.py
+++ prac_07/project.py
@@ -9,3 +9,17 @@ class Project:
         self.cost_estimate = cost_estimate
         self.completion = completion
 
+    def is_complete(self):
+        return self.completion == 100
+
+    def __lt__(self, other):
+        """This method will sort projects by priority"""
+        return self.priority < other.priority
+
+    def filter_date(self, filter_date):
+        """This method will sort projects by date"""
+        return self.start_date > filter_date
+
+    def __str__(self):
+        """Return a string with name, date, priority, cost and completion"""
+        return f"{self.name}, {self.start_date}, {self.priority}, {self.cost_estimate}, {self.completion}"
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,60 +1,118 @@
 """CP1404 Prac 07 Project Management"""
-"""Estimated time: 120min"""
-"""Actual time: min"""
+from sys import intern
+
+from Tools.scripts.dutree import display
+
+"""Estimated time: 150min"""
+"""Actual time: 190min"""
 
 from prac_07.project import Project
-
-MAIN_MENU = """(L)oad project
-(S)ave project
-(D)isplay project
-(F)ilter projects by date
-(A)dd new project
-(U)pdate project
-(Q)uit"""
-FILENAME = 'projects.txt'
+import datetime
+MAIN_MENU = """- (L)oad project
+- (S)ave project
+- (D)isplay project
+- (F)ilter projects by date
+- (A)dd new project
+- (U)pdate project
+- (Q)uit"""
 
 def main():
     """Display, filter, modify or add projects to a file"""
+    print("Welcome to Pythonic Project Management")
+    filename = "projects.txt"
+    projects = load_projects(filename)
+    number_of_projects = len(projects)
+    print(f"Loaded {number_of_projects} projects from {filename}")
     print(MAIN_MENU)
     choice = input(">>> ").upper()
+    projects = load_projects(filename)
     while choice != "Q":
         if choice == "L":
-            projects = load_projects()
-        # elif choice == "S":
-        #
-        # elif choice == "D":
-        #
-        # elif choice == "F":
-        #
-        # elif choice == "A":
-        #
-        # elif choice == "U":
+            print("Please enter filename")
+            filename = input(">>> ")
+            projects = load_projects(filename)
+            print("")
+            print(MAIN_MENU)
+            choice = input(">>> ").upper()
+        elif choice == "S":
+            save_projects(projects, filename)
+            print("")
+            print(MAIN_MENU)
+            choice = input(">>> ").upper()
+        elif choice == "D":
+            display_projects(projects)
+            print("")
+            print(MAIN_MENU)
+            choice = input(">>> ").upper()
+        elif choice == "F":
+            filter_projects(projects)
+            print("")
+            print(MAIN_MENU)
+            choice = input(">>> ").upper()
+        elif choice == "A":
+            add_project(projects)
+            print("")
+            print(MAIN_MENU)
+            choice = input(">>> ").upper()
+        elif choice == "U":
+            update_project(projects)
+            print("")
+            print(MAIN_MENU)
+            choice = input(">>> ").upper()
+        else:
+            print("Invalid input")
+            print(MAIN_MENU)
+            choice = input(">>> ").upper()
+    save_choice = input(f"Would you like to save {filename}? ").upper()
+    if save_choice == "Y":
+        save_projects(projects, filename)
+        print()
+    else:
+        print("Thank you for using custom-built project management software.")
 
-
-
-
-
-
-
-def load_projects():
+def load_projects(filename):
     """Load projects from csv file"""
     projects = []
-    in_file = open(FILENAME, "r")
+    in_file = open(filename, "r")
+    next(in_file)
     for line in in_file:
-        line.strip()
+        parts = line.strip().split("\t")
+        start_date = datetime.datetime.strptime(parts[1], "%d/%m/%Y").date()
+        project_details = Project(parts[0], start_date, int(parts[2]), float(parts[3]), int(parts[4]))
+        projects.append(project_details)
+    in_file.close()
+    return projects
 
+def save_projects(projects, filename):
+    """Save projects to file"""
+    save_file_projects = []
+    for project in projects:
+        parts_0 = [project.name, str(project.start_date), str(project.priority), str(project.cost_estimate),
+                   str(project.completion)]
+        parts_1 = " ".join(parts_0)
+        save_file_projects.append(parts_1)
+    for project in save_file_projects:
+        print(project)
+    with open(f"{filename}", "w") as out_file:
+        for project in save_file_projects:
+            out_file.write(project + "\n")
+    return
 
-def display_projects():
-    """Display projects"""
-
-def filter_projects():
-    """Filter projects by user inputted date"""
-
-def add_project():
-    """Add new project"""
-
-def update_project():
-    """Update project"""
+def display_projects(projects):
+    """Display loaded projects"""
+    uncompleted_projects = [project for project in projects if not project.is_complete()]
+    uncompleted_projects.sort()
+    completed_projects = [project for project in projects if project.is_complete()]
+    completed_projects.sort()
+    print("Incomplete projects:")
+    for project in uncompleted_projects:
+        print(f"  {project.name}, {project.start_date.strftime('%d/%m/%Y')}, priority {project.priority}"
+              f", estimate: ${project.cost_estimate}, completion: {project.completion}%")
+    print("Completed projects:")
+    for project in completed_projects:
+        print(f"  {project.name}, {project.start_date.strftime('%d/%m/%Y')}, priority {project.priority}"
+              f", estimate: ${project.cost_estimate}, completion: {project.completion}%")
+    return
 
 
 
