c1affd87d8b0cde38b82e24df9c345297cb687a9
MATWILL19 <mathew.wilington@my.jcu.edu.au>
2024-11-19 19:51:02 +1000

    Finish convert_miles_km.kv program, add convert_mil, convert_number and handle_increment functions to convert_miles_km.py

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,17 +1,31 @@
 """CP1404 - prac_08 - convert miles to kilometres"""
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 class ConvertDistance(App):
     """Convert miles to kilometres"""
+    output_km = StringProperty()
 
     def build(self):
         """Construct the app"""
         self.title = "Convert miles to kilometers program"
         self.root = Builder.load_file('convert_miles_km.kv')
+        self.output_km = "Convert miles to kilometers"
         return self.root
 
     def convert_mil_to_km(self):
+        """Convert miles to kilometres"""
+        self.root.ids.output_label.text = f"{self.root.ids.input_number.text}"
+
+    def handle_increment(self, text, increment):
+        """Increase miles by 1"""
+        miles = self.convert_mil_to_km(text) + increment
+
+    def convert_number(text):
+        """Convert text to number"""
+        try:
+            return int()
 
 
 ConvertDistance().run()
