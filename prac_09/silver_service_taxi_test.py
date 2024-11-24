36bddbd66a341933b2b5013ed0ca0d433ddcabd0
MATWILL19 <mathew.willington@my.jcu.edu.au>
2024-11-24 20:11:47 +1000

    Refactor silver_service_taxi_test

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,6 +1,7 @@
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
 my_taxi= SilverServiceTaxi("Hummer", 200, 4)
-my_taxi.drive(100)
+print(my_taxi)
+my_taxi.drive(18)
 print(my_taxi)
 print(my_taxi.get_fare())
